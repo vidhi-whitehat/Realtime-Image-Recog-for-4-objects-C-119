a0c98724cf5ca0cdacce1b302a7515fde27fc356
vidhi-whitehat <76781620+vidhi-whitehat@users.noreply.github.com>
2021-07-03 15:40:50 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,26 @@
+function setup() {
+    canvas = createCanvas(300, 300);
+    canvas.center();
+    video = createCapture(VIDEO);
+    video.hide();
+    classifier = ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/v_sl95BzE/model.json', modelLoaded);
+}
+
+function modelLoaded() {
+    console.log(modelLoaded);
+}
+
+function draw() {
+    image(video, 0, 0, 300, 300);
+    classifier.classify(video, gotResult);
+}
+
+function gotResult(error, result) {
+    if(error) {
+        console.error(error);
+    } else {
+        console.log(result);
+        document.getElementById("result_object_name").innerHTML = result[0].label;
+        document.getElementById("result_object_accuracy").innerHTML = result[0].confidence.toFixed(3);
+    }
+}
